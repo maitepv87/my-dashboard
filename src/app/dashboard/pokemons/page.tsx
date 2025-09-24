d6cbc4eb366d58d94a271101cd66f2213a8a7740
Maite Perez Vento <maitepv87@gmail.com>
2025-09-24 13:32:59 -0400

    Add Error Page

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -10,6 +10,8 @@ const getPokemos = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => {
     name: pokemon.name,
   }));
 
+  // throw new Error("NO deberia pasar");
+
   return pokemons;
 };
 
